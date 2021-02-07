7ec9bbcd488071a145bed8457fd46d1a78df0d4e
Jordan Raychev <jpraychev@gmail.com>
2021-02-07 07:28:16 +0200

    Remove function with 0 and 1 childs

--- binary-trees/main.py
+++ binary-trees/main.py
@@ -1,34 +1,22 @@
 import random
 
-# The node class
 class Node():
 
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
+        self.parent = None
 
     def __str__(self):
         return str(self.data)
-
-# t_root = Node(5)
-# t_1 = Node(4)
-# t_2 = Node(6)
-# t_root.left = t_1
-# t_root.right = t_2
-
-# print(t_root.left)
-# print(t_root.right)
 
 class Tree():
 
     def __init__(self):
         self.root = None
-        self.l_height = 0
-        self.r_height = 0
 
     def add(self, data):
-
         # If tree is empty
         if self.root == None:
             self.root = Node(data)
@@ -36,22 +24,20 @@ class Tree():
             self._add(self.root, data)
 
     def _add(self, current_node, data):
-        
         if data > current_node.data:
             if current_node.right == None:
                 current_node.right = Node(data)
+                current_node.right.parent = current_node
             else:
                 self._add(current_node.right, data)
         elif data < current_node.data:
             if current_node.left == None:
                 current_node.left = Node(data)
+                current_node.left.parent = current_node
             else:
                 self._add(current_node.left, data)
         else:
             print('We have such item') 
-
-    def remove(self, data):
-        pass
 
     def find_min(self):
         return self._find_min(self.root)
@@ -59,7 +45,6 @@ class Tree():
     def _find_min(self, current_node):
         if current_node.left == None:
             return current_node.data
-        self.l_height += 1
         return self._find_min(current_node.left)
 
     def find_max(self):
@@ -68,18 +53,73 @@ class Tree():
     def _find_max(self, current_node):
         if current_node.right == None:
             return current_node.data
-        self.r_height += 1
         return self._find_max(current_node.right)
 
     def left_height(self):
-        self.find_min()
-        return self.l_height
+        if self.root == None:
+            return 0
+        return self._left_height(self.root, 0)
+    
+    def _left_height(self,current_node,current_height):
+        if current_node == None: 
+            return current_height
+        return self._left_height(current_node.left,current_height+1)
 
     def right_height(self):
-        self.find_max()
-        return self.r_height
+        if self.root == None:
+            return 0
+        return self._right_height(self.root, 0)
+    
+    def _right_height(self,current_node,current_height):
+        if current_node == None: 
+            return current_height
+        return self._right_height(current_node.right,current_height+1)        
 
+    def search(self, data):
+        if self.root == None:
+            return None
+        return self._search(self.root, data)
 
+    def _search(self, current_node, data):
+        if current_node.data == data:
+            return current_node
+        elif current_node.data < data and current_node.right != None:
+            return self._search(current_node.right, data)
+        elif current_node.data > data and current_node.left != None:
+            return self._search(current_node.left, data)            
+
+    def remove(self, data):
+
+        # If node to be deleted has no childs
+        # Find the node and delete it
+        node = self.search(data)
+        parent = node.parent
+        if node.left == None and node.right == None:
+            # Children 0 - safely delete the node
+            if parent.left == node: 
+                parent.left = None
+            elif parent.right == node:
+                parent.right = None
+        elif node.left != None and node.right != None:
+            print('node has two childs')
+        else:
+            # Children 1 - point parent (left or right) to the child of deleted node
+            node = self.search(data) 
+            parent = node.parent
+
+            if parent.left == node:
+                if node.left:
+                    parent.left = node.left
+                elif node.right:
+                    parent.left = node.right
+                node = None
+            elif parent.right == node:
+                if node.left:
+                    parent.right = node.left
+                elif node.right:
+                    parent.right = node.right
+                node = None 
+                
 tree = Tree()
 
 tree.add(50)
@@ -93,15 +133,8 @@ tree.add(90)
 tree.add(100)
 tree.add(110)
 tree.add(35)
-
-# for _ in range(1,10):
-#     value = random.randint(1,6789)
-#     tree.add(int(value))
-
-# print(tree.find_max())
-# print(tree.find_min())
-
-print(tree.right_height())
-print(tree.left_height())
+tree.add(95)
+tree.add(94)
+tree.add(15)
 
 
